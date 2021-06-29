1abe54089a34329c51b2908fbaf43050881cf4fd
Joshan Pradhan <joshan.pradhan@apexcollege.edu.np>
2021-06-29 18:13:20 +0545

    Donate from listing done

--- src/screens/Donor/Home/Receivers/Receivers.js
+++ src/screens/Donor/Home/Receivers/Receivers.js
@@ -6,19 +6,34 @@ import {
   TextInput,
   TouchableOpacity,
   ActivityIndicator,
+  Modal,
+  View,
+  Dimensions
 } from "react-native";
 import * as theme from "../../../../constants/theme.js";
 import {
   Block,
   Text,
   Empty,
+  ErrorMessage,
   ReceiversDetail,
+  CustomActivityIndicator,
 } from "../../../../components/Index.js";
-import Dummy from "./Dummy.js";
 import { Ionicons } from "@expo/vector-icons";
+import { Formik } from "formik";
+import { ManualValidationSchema } from "./../../../../utility/ValidationSchema.js";
+import PaymentProto from "./../../../../protos/payment_pb";
+import TickIconComponent from "./../../../../assets/icons/tickIconComponent.js";
 
-const Receivers = ({ navigation, data, receivers }) => {
+const WIDTH = Dimensions.get("window").width;
+
+const Receivers = ({ navigation, data, loginData,receivers,manualDonateConfirmationStart,manualDonateConfirmationClear,manualDonateConfirmationData }) => {
   const [search, setSearch] = useState();
+  const [accountData, setAccountData] = useState();
+  const [amountFocus, setAmountFocus] = useState();
+  const [remarksFocus, setRemarksFocus] = useState();
+  const [confirmationSuccessfulVisible, setConfirmationSuccessfulVisible] = useState(false);
+  const [confirmationMessageVisible, setConfirmationMessageVisible] = useState(false);
   const [filteredDataSource, setFilteredDataSource] = useState(
     data.receivers != null ? data.receivers.clientsList : null
   );
@@ -35,6 +50,17 @@ const Receivers = ({ navigation, data, receivers }) => {
     }
   }, [data.receivers]);
 
+  useEffect(() => {
+    if (manualDonateConfirmationData.manualDonateConfirmation !== null) {
+      if (manualDonateConfirmationData.manualDonateConfirmation.success) {
+        console.log("Success")
+        setConfirmationSuccessfulVisible(false);
+        setConfirmationMessageVisible(true);
+        manualDonateConfirmationClear();
+      }
+    }
+  }, [manualDonateConfirmationData.manualDonateConfirmation]);
+
   function searchFilterFunction(text) {
     if (text) {
       const newData = masterDataSource.filter(function (item) {
@@ -49,6 +75,168 @@ const Receivers = ({ navigation, data, receivers }) => {
       setSearch(text);
     }
   }
+   
+  const onSubmitDonateConfirmation = (values) => {
+    const donationProto = new PaymentProto.Transaction();
+    donationProto.setDonoraccountid(loginData.user.account.accountid);
+    donationProto.setReceiveraccountid(accountData.account.accountid);
+    donationProto.setAmount(values.amount);
+    donationProto.setRemark(values.remarks);
+    donationProto.setTransactionmedium(PaymentProto.TransactionMedium.INTERNAL_MEDIUM);
+    donationProto.setTransactiontype(PaymentProto.TransactionType.DONATE_FUND);
+    donationProto.setTransactionstatus(PaymentProto.TransactionStatus.TRANSACTION_APPROVED);
+    manualDonateConfirmationStart(donationProto);
+    setConfirmationSuccessfulVisible(false)
+  };
+
+  const ConfirmationMessage = () => (
+    <SafeAreaView>
+      <Modal
+        visible={confirmationMessageVisible}
+        transparent={true}
+        animationType="fade"
+        statusBarTranslucent={true}
+        onRequestClose={() =>
+          setConfirmationMessageVisible(false)
+        }
+      >
+        <View style={styles.container}>
+          <View style={[styles.modal, { width: WIDTH - 45 }]}>
+            <Text center style={{ fontSize: 18, fontWeight: "700" }}>
+              Donation Successfull!
+            </Text>
+            <View style={{ paddingVertical: 25, alignItems: "center" }}>
+             <TickIconComponent/>
+            </View>
+            <View style={{ paddingHorizontal: 30 }}>
+              <Button onPress={() => setConfirmationMessageVisible(false)}>
+                <Text button style={{ fontSize: 18 }}>
+                  Got It!
+                </Text>
+              </Button>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
+
+  const DonateModal = () => (
+    <SafeAreaView>
+      <Modal
+        visible={confirmationSuccessfulVisible}
+        transparent={true}
+        animationType="fade"
+        statusBarTranslucent={true}
+        onRequestClose={() =>
+          setConfirmationSuccessfulVisible(!confirmationSuccessfulVisible)
+        }
+      >
+        <View style={styles.container}>
+          <View style={[styles.modal, { width: WIDTH - 45 }]}>
+            <Formik
+          initialValues={{
+            amount: "",
+            remarks:" "
+          }}
+          onSubmit={(values) => {
+            onSubmitDonateConfirmation(values);
+          }}
+          validationSchema={ManualValidationSchema}
+        >
+          {({
+            handleChange,
+            touched,
+            setFieldTouched,
+            handleSubmit,
+            values,
+            errors,
+          }) => (
+            <>
+
+              <Input
+                label="Receiver Name"
+                autoCapitalize="words"
+                value={accountData.account.fullname}
+                editable={false}
+              />
+              
+              <Input
+                label="Amount"
+                focus={amountFocus}
+                onChangeText={handleChange("amount")}
+                onBlur={() => {
+                  setFieldTouched("amount");
+                  setAmountFocus(false);
+                }}
+                number
+                onFocus={() => setAmountFocus(true)}
+                value={values.amount}
+                style={{
+                  borderBottomColor: amountFocus
+                    ? theme.colors.primary2
+                    : touched.amount && errors.amount
+                    ? theme.colors.red
+                    : theme.colors.solidGray,
+                }}
+              />
+              <ErrorMessage error={errors.amount} visible={touched.amount} />
+               <Input
+                label="Remarks"
+                autoCapitalize="sentences"
+                focus={remarksFocus}
+                onChangeText={handleChange("remarks")}
+                onBlur={() => {
+                  setFieldTouched("remarks");
+                  setRemarksFocus(false);
+                }}
+                onFocus={() => setRemarksFocus(true)}
+                value={values.remarks}
+                style={{
+                  borderBottomColor: remarksFocus
+                    ? theme.colors.primary2
+                    : touched.remarks && errors.remarks
+                    ? theme.colors.red
+                    : theme.colors.solidGray,
+                }}
+              />
+              <ErrorMessage error={errors.remarks} visible={touched.remarks} />
+              <Block style={{ flex: 0, paddingVertical: 10 }}>
+                {!errors.receiverName && !errors.amount ? (
+                  <Button onPress={handleSubmit}>
+                    {manualDonateConfirmationData.isLoading ? (
+                      <>
+                       <CustomActivityIndicator
+                      label="Requesting..."
+                      isLoading={manualDonateConfirmationData.isLoading}
+                    />
+                     <Text button style={{ fontSize: 18 }}>
+                        Proceed
+                      </Text>
+                      </>
+                    ) : (
+                      <Text button style={{ fontSize: 18 }}>
+                        Proceed
+                      </Text>
+                    )}
+                  </Button>
+                ) : (
+                  <Button>
+                    <Text button style={{ fontSize: 18 }}>
+                      Proceed
+                    </Text>
+                  </Button>
+                )}
+              </Block>
+            </>
+          )}
+        </Formik>
+             
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
+  );
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -106,13 +294,18 @@ const Receivers = ({ navigation, data, receivers }) => {
                   profilePic={post.item.profilepic}
                   name={post.item.account.fullname}
                   clientType={post.item.clienttype}
-                  onPress={() => navigation.navigate("Donate")}
+                  onPress={() => {
+                        setConfirmationSuccessfulVisible(!confirmationSuccessfulVisible)
+                        setAccountData(post.item);
+                  }}
                 />
               </Block>
             )}
           />
         </Block>
       )}
+      {DonateModal()}
+      {ConfirmationMessage()}
     </SafeAreaView>
   );
 };
@@ -148,6 +341,20 @@ const styles = StyleSheet.create({
     flex: 0,
     borderRadius: 2,
   },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(52, 52, 52, 0.8)",
+  },
+  modal: {
+    borderRadius: 4,
+    borderColor: theme.colors.gray,
+    backgroundColor: theme.colors.white,
+    paddingVertical: 20,
+    paddingHorizontal:16
+  },
+
 });
 
 export default Receivers;
--- src/screens/Donor/Home/Receivers/index.js
+++ src/screens/Donor/Home/Receivers/index.js
@@ -4,15 +4,17 @@ import { createStructuredSelector } from "reselect";
 import { compose } from "redux";
 import Receivers from "./Receivers";
 import { receiversStart } from "./actions";
-
+import { manualDonateConfirmationStart,manualDonateConfirmationClear } from "./../ManualDonateConfirmation/actions";
 const mapStateToProps = createStructuredSelector({
 	data: (state) => state.receivers,
 	loginData: (state) => state.login,
-	
+	manualDonateConfirmationData: (state) => state.manualDonateConfirmation,
 });
 const mapDispatchToProps = (dispatch) => {
 	return {
 		receivers: () => dispatch(receiversStart()),
+		manualDonateConfirmationStart: (values) => dispatch(manualDonateConfirmationStart(values)),
+		manualDonateConfirmationClear: () => dispatch(manualDonateConfirmationClear()),
 	};
 };
 
